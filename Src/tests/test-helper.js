051a009d8546e9c4c6301b5e998bcd7a5e0b364e
Manh Nguyen <manh.nguyen@thirdsight.net>
2025-06-05 16:16:51 +0800

    Starter Modern Ember Template

---
+++ Src/tests/test-helper.js
@@ -0,0 +1,14 @@
+import Application from 'starter-modern-ember-template/app';
+import config from 'starter-modern-ember-template/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { loadTests } from 'ember-qunit/test-loader';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+setupEmberOnerrorValidation();
+loadTests();
+start();
